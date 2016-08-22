c08a19894029780b207a936a4027ec5a5bc602c4
Fumiya Amano <fumiya.amano@gree.co.jp>
2016-08-23 01:06:00 +0900

    update babel.sh

--- babel.sh
+++ babel.sh
@@ -1,5 +1,5 @@
-ES6_DIR="js"
-ES5_DIR="js"
+ES6_DIR="game_assets/js"
+ES5_DIR="game_assets/js"
 
 if [ $# -ne 1 ]; then
   echo "指定された引数は$#個です。" 1>&2
@@ -14,7 +14,11 @@ mkdir -p ${1}/${ES5_DIR}
 babel ${ES6_DIR}/ -o ${FILE_PATH}
 sed -i '' -e "s/^importScript/\/\/importScript/g" ${FILE_PATH}
 
-cp -r images ${1}/
+rm -r ${1}/game_assets/images/
+cp -r game_assets/images ${1}/game_assets/
+rm -r ${1}/game_assets/css/
+cp -r game_assets/css ${1}/game_assets/
+
 
 # babel $ES6_DIR"/" -o "js/main_es5.js"
 #
